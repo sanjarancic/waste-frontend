2c144a4d1d1b17db945a7710324ce04807a6a19f
Sanja Rancic <sanjarancic0@gmail.com>
2020-12-15 22:09:00 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+async function uploadPhoto(photo) {
+  const formData = new FormData();
+  formData.append('file', photo)
+  const response = await fetch(`http://192.168.0.17:5000/predict`, {method: 'POST', body: formData});
+  return await response.json();
+}
+
+
 function App() {
+
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  async function handleUpload(evt) {
+    setError('');
+    setResult(null);
+    setLoading(true);
+    try {
+      const res = await uploadPhoto(evt.target.files[0]);
+      setResult(res);
+    } catch (error) {
+      alert(error);
+      setResult(null);
+      setError('error uploading picture')
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+
+        {loading && <div className="loading"></div>}
+
+        {result && !loading && (
+          <div className="result">
+            <img src={`${result.class}.jpeg`} alt={result.class} />
+            <div className="result-class">{result.class}</div>
+            <div className="result-prob">{Number(result.probability*100).toFixed(2)}%</div>
+          </div>
+        )}
+        {!result && !loading && <div className="error">{error}</div>}
+
+        <input className="btn" type="file" accept="image/*" onChange={handleUpload} />
+      </div>
+
+      {/* <button className="btn">Take photo</button> */}
     </div>
   );
 }
